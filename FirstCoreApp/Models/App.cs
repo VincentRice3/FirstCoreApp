e114c5ff1b8cd7f45cb00b2f03300996349b0068
ricev <ricev@M16485>
2023-10-25 10:11:34 +0100

    Add project files.

---
+++ FirstCoreApp/Models/App.cs
@@ -0,0 +1,36 @@
+﻿namespace FirstCoreApp.Models
+{
+    public class App
+    {
+        public string AppId { get; private set; }
+        public string AppName { get; private set; }
+        public string Url { get; private set; }
+        public string Colour { get; private set; }
+
+        public App (string id, string appName, string colour)
+        {
+            AppId = id;
+            AppName = appName;
+            Url = GetUrl(AppName);
+            Colour = colour;
+        }
+
+        private static string GetUrl(string app)
+        {
+            string url = $"https://{app}.com";
+            return url ;    
+        }
+
+        public string Stringify()
+        {
+            //Console.WriteLine($"App Name: {AppName} \nURL: {Url} \nColour: {Colour}");
+
+            return AppName;
+        }
+
+        public string GetAppColour()
+        {
+            return Colour;
+        }
+    }
+}
---
+++ FirstCoreApp/Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstCoreApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+
+        public string Greeting { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+
+        public void OnGet()
+        {
+            Name = "Vincent Rice";
+            Greeting = GetGreeting();
+            Time = GetTime();
+
+        }
+
+
+        public string GetGreeting() {
+            TimeSpan h = DateTime.Now.TimeOfDay;
+            if (h.Hours >= 0 && h.Hours <=11) {
+                return "Good Morning";
+            }else if(h.Hours >= 11 && h.Hours <= 16) 
+            {
+                return "Good Afternoon";
+            } else if(h.Hours >= 16 && h.Hours <= 23)
+            {
+               return "Good Evening";
+            }
+            else
+            {
+                return "invalid greeting";
+            }
+        }
+
+        public string GetTime()
+        {
+            TimeSpan t = DateTime.Now.TimeOfDay;
+
+            Time = $"{t.Hours}:{t.Minutes:00}";
+            
+            return Time;
+        }
+
+        public string GetColour()
+        {
+            char[] values = { 'A', 'B', 'C', 'D', 'E', 'F', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+            string hexValue = "#";
+
+
+            for (int i  = 0; i < 6; i++)
+            {
+                Random randomNumber = new();
+
+                int num = randomNumber.Next(values.Length);
+
+                hexValue += values[num];
+
+            }
+
+            return hexValue;
+        }
+    }
+}
